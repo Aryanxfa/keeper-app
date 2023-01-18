8abbf22a3677dbaf5b9f5b4dbe8acaae531a6796
Aryanxfa <aryan200152@gmail.com>
2023-01-18 11:22:45 +0530

    newly added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,10 +3,14 @@ import { Header } from "./Header";
 import { Footer } from "./Footer";
 import { Note } from "./Note.jsx";
 import { notes } from "../notes.js";
+import { Add_notedb } from "./Routes";
+const connectDb = require('./Database');
 
 console.log(notes);
 
 export function App() {
+    
+    connectDb();
 
     return (
         <div>
@@ -18,6 +22,7 @@ export function App() {
                     content={noteitem.content}
                 />
             ))}
+            <button onClick={Add_notedb()}>Add Note</button>
             <Footer />
         </div>
     );
---
+++ src/components/Routes.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import mongoose from "mongoose";
+
+
+export function Add_notedb() {
+
+
+}
+
+export function Get_notedb() {
+
+
+}
